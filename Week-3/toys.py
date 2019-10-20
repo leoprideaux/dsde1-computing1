613eebde59ba92ab573d56114c63d3882c5f6c2f
Leo Prideaux <leo.planck-prideaux19@imperial.ac.uk>
2019-10-20 22:26:09 +0100

    edits to toy.py

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -9,36 +9,46 @@ with functions.
 # write a function that adds 1
 # to the input and prints the result
 def inc(a):
+    a = a + 1
     print(a)
 
+inc(5)
 
 # write a function that adds 1
 # to the input and returns the result
 def inc_return(a):
-    return # hint this is incomplete
+    return a + 1 # hint this is incomplete
 
+print(inc_return(10))
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a, b):
-    return
+    sum_ab = a + b
+    return sum_ab
 
+print(sum(20, 15))
 
 # write a function that takes two
 # numbers, adds them together using 
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    sum2 = sum(a, b)
+    sum2 = inc_return(sum2)
+    return sum2
 
+print(sum_inc(56, 94))
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 def is_even(a):
-    return
+    eventest = a % 2 == 0
+    return eventest
 
+print(is_even(12))
 
 # create for loop that takes a string
 # and an integer and returns a new string 
@@ -47,7 +57,12 @@ def is_even(a):
 # e.g. string_repeat('ho', 3) returns
 # 'hohoho'
 def string_repeat(phrase, repeat):
+    
+    s = str()
+    for sentense in range(repeat):
+        s += phrase
+    print(s)    
     # hint: you can add strings together 
     # in order to concatenate them
-    return
 
+string_repeat('ha', 3)
